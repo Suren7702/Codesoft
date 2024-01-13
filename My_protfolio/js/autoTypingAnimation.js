cb0fdfda8283d6817da421654f6d2b9ee7eb33dd
Surendhar Rajasekaran <suren7702@gmail.com>
2024-01-13 07:58:36 +0530

    Add files via upload

---
+++ My_protfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi , I am Surendhar I am Studing In Bishop Heber College in Trichy-621005  , I am Begginer Level Web Devoloper  this is My First Edited Website"
+      ],
+      typeSpeed: 6,
+      contentType: 'html'
+    });
+  }, 100);
